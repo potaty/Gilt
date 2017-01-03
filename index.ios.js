702a134c8108558797264918cbfce515a7d80f7e
potaty <potatyangel@sina.com>
2017-01-04 00:50:01 +0800

    second commit

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -13,27 +7,7 @@ import {
   Alert,
   View
 } from 'react-native';
-
-
-const loginHandler = () => {
-  var myHeaders = new Headers();
-
-  var myInit = { method: 'GET',
-                 headers: myHeaders,
-                 mode: 'cors',
-                 cache: 'default' };
-
-  fetch('https://api.github.com/authorizations',myInit)
-  .then(function(response) {
-    console.warn(response)
-  })
-  .then(function(myBlob) {
-    var objectURL = URL.createObjectURL(myBlob);
-    myImage.src = objectURL;
-  });
-  return 'fetch'
-};
-
+import Profile from './src/pages/profile.js';
 
 export default class GithubClient extends Component {
   constructor(props) {
@@ -43,45 +17,9 @@ export default class GithubClient extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={{height: 30, marginHorizontal: 10, marginVertical: 10, borderColor: 'gray', textAlign: 'center'}}
-          value={"username"}
-          onChangeText={(username) => this.setState({username})}
-          value={this.state.username}
-          autoFocus={true}
-        />
-        <TextInput
-          style={{height: 30, marginHorizontal: 10, marginVertical: 10  , borderColor: 'gray', textAlign: 'center'}}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-          autoFocus={false}
-        />
-        <Button title="Login"
-                onPress={loginHandler}/>
-        <Button title="Regist"/>
-      </View>
+      <Profile/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('GithubClient', () => GithubClient);
---
+++ src/pages/profile.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  TextInput,
+  Button,
+  Alert,
+  Text,
+  Image,
+  View,
+  ToolbarAndroid,
+} from 'react-native';
+
+console.log(ToolbarAndroid)
+
+const loginHandler = () => {
+  var myHeaders = new Headers();
+
+  var myInit = { method: 'GET',
+                 headers: myHeaders,
+                 mode: 'cors',
+                 cache: 'default' };
+
+  fetch('https://api.github.com/authorizations',myInit)
+  .then(function(response) {
+    console.warn(response)
+  })
+  .then(function(myBlob) {
+    var objectURL = URL.createObjectURL(myBlob);
+    myImage.src = objectURL;
+  });
+  return 'fetch'
+};
+
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: 'username', password: 'password'};
+  }
+  onActionSelected(position) {
+    if (position === 0) { // index of 'Settings'
+      showSettings();
+    }
+  }
+  render() {
+    return (
+      <View>
+        <ToolbarAndroid style={styles.toolbar}
+          title="Profile"
+          actions={[{title: 'Settings', icon: require('../../pic/head.png'), show: 'always'}]}
+          onActionSelected={this.onActionSelected} />
+        <View style={styles.container}>
+          <Image
+            style={styles.head}
+            source={require('../../pic/head.png')}
+          />
+          <Text value={"Magica"}></Text>
+          <Button title="Repositor"/>
+          <Button title="Regist"/>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  head: {
+    height: 50,
+    width: 50,
+  },
+  toolbar: {
+    backgroundColor: '#e9eaed',
+    height: 56,
+  }
+});
