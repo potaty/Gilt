d4bb906df15f06678df4486241c84efb523cd537
potaty <potatyangel@sina.com>
2017-01-08 04:17:26 +0800

    add issue page

--- src/pages/issue.js
+++ src/pages/issue.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Alert, AppRegistry, BackAndroid, Button, Navigator, StyleSheet, TextInput, ToolbarAndroid, View, Text, ListView } from 'react-native'
+import { Button, Image, StyleSheet, TextInput, ToolbarAndroid, View, Text, ListView } from 'react-native'
+
+import Qingzhen from '../images/qingzhen.png'
+import Comment from '../components/comment'
 
 const styles = StyleSheet.create({
   toolbar: {
@@ -13,22 +16,79 @@ const styles = StyleSheet.create({
     marginRight: 20,
     fontSize: 20,
   },
-  issueTitle: {
+  message: {
     flexDirection: 'row',
+    marginHorizontal: 20,
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
+  author: {
+    fontWeight: 'bold',
+  },
+  branch: {
+    backgroundColor: '#b3e5fc',
+    borderRadius: 3,
+    color: '#01579b',
+  },
+  description: {
+    margin: 10,
+    backgroundColor: '#eee',
+    padding: 20,
+  },
+  detail: {
+    flexDirection: 'row',
+    margin: 20,
+  },
+  head: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+  },
+  input: {
+    borderColor: 'gray',
+    margin: 20,
   },
 })
 
 export default class Issue extends React.Component {
-  state = {}
+  state = {
+    text: 'Useless Placeholder',
+  }
   render() {
     return (<View>
       <ToolbarAndroid style={styles.toolbar}
         title={this.props.route.login || 'Issue'} titleColor="#ffffff" />
       <View>
-        <Button title={'⚠️ Open'} color="#64dd17"></Button>
-        <View style={styles.issueTitle}>
-          <Text style={styles.title}>Update to Table.alias method (#5)</Text>
+        <View>
+          <View>
+            <Button title={'⚠️ Open'} color="#00c853"></Button>
+            <Button title={'Click me to see the changes in code.'} color="#81d4fa" style={styles.code}></Button>
+          </View>
         </View>
+        <Text style={styles.title}>Update to Table.alias method (#5)</Text>
+        <View style={styles.detail}>
+          <Image source={Qingzhen} style={styles.head} />
+          <View style={styles.message}>
+            <Text style={styles.author}>{'potaty'}</Text>
+            <Text> wants to merge 1 commit into </Text>
+            <Text style={styles.branch}> brynary:master </Text>
+            <Text> from </Text>
+            <Text style={styles.branch}> aliceatlas:master </Text>
+          </View>
+        </View>
+        <View style={styles.description}>
+          <Text>Change the unstandable part in guide.</Text>
+        </View>
+        <Comment pic={Qingzhen} author={'potaty'} time={'one hour ago'} comment={'please dont lasdasidjiasjdiasjdiajsidjasidjiasjdiasjidjasidjiasjdiaugh.'}></Comment>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state}
+          multiline = {true}
+          numberOfLines = {2}
+        />
+        <Button title={'Comment'}></Button>
       </View>
     </View>)
   }
